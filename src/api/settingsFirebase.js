3a05a28c86c76e29373c6d1b8fccc213e94330f7
Pool1994 <poolveracubas1994@gmail.com>
2021-09-30 00:06:47 -0500

    #

---
+++ src/api/settingsFirebase.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyCH-fBey-Ccs70eGGpLGFuryJaAko5JVV8",
+    authDomain: "vue-trello-6a86f.firebaseapp.com",
+    projectId: "vue-trello-6a86f",
+    storageBucket: "vue-trello-6a86f.appspot.com",
+    messagingSenderId: "579961573615",
+    appId: "1:579961573615:web:2f9cb25b5e22d77438cb1a"
+}
